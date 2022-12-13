7b90e2333e07101a09ad1b8e6a62240978203325
MYasser <49511207+myasser63@users.noreply.github.com>
2022-12-13 23:28:42 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -1613,3 +1613,22 @@ class RepC3(nn.Module):
 
     def forward(self, x):
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
+    
+ class RepC3_SE_PRE(nn.Module):
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        self.SE = SE_Block(c1)
+
+    def forward(self, x):
+        x1 = self.SE(x)
+        x1 = self.m(self.cv1(x))
+        x2 = self.cv2(x)
+        x = self.cv3(torch.cat((x1, x2), 1))
+
+        return x   
