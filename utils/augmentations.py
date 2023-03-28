d1fa55f0056f0123ecb30a52a95362d99d9c14c4
MYasser <49511207+myasser63@users.noreply.github.com>
2023-03-29 00:00:54 +0800

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -18,10 +18,35 @@ IMAGENET_MEAN = 0.485, 0.456, 0.406  # RGB mean
 IMAGENET_STD = 0.229, 0.224, 0.225  # RGB standard deviation
 
 
+# class Albumentations:
+#     # YOLOv5 Albumentations class (optional, only used if package is installed)
+#     def __init__(self):
+#         self.transform = None
+#         prefix = colorstr('albumentations: ')
+#         try:
+#             import albumentations as A
+#             check_version(A.__version__, '1.0.3', hard=True)  # version requirement
+
+#             T = [
+#                 A.Blur(p=0.01),
+#                 A.MedianBlur(p=0.01),
+#                 A.ToGray(p=0.01),
+#                 A.CLAHE(p=0.01),
+#                 A.RandomBrightnessContrast(p=0.0),
+#                 A.RandomGamma(p=0.0),
+#                 A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
+#             self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+
+#             LOGGER.info(prefix + ', '.join(f'{x}'.replace('always_apply=False, ', '') for x in T if x.p))
+#         except ImportError:  # package not installed, skip
+#             pass
+#         except Exception as e:
+#             LOGGER.info(f'{prefix}{e}')
 class Albumentations:
     # YOLOv5 Albumentations class (optional, only used if package is installed)
-    def __init__(self):
+    def __init__(self, num_aug=3):
         self.transform = None
+        self.num_aug = num_aug
         prefix = colorstr('albumentations: ')
         try:
             import albumentations as A
@@ -35,6 +60,10 @@ class Albumentations:
                 A.RandomBrightnessContrast(p=0.0),
                 A.RandomGamma(p=0.0),
                 A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
+
+            # Repeat each transform num_aug times
+            T = [t for t in T for _ in range(num_aug)]
+
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
             LOGGER.info(prefix + ', '.join(f'{x}'.replace('always_apply=False, ', '') for x in T if x.p))
@@ -42,7 +71,7 @@ class Albumentations:
             pass
         except Exception as e:
             LOGGER.info(f'{prefix}{e}')
-
+            
     def __call__(self, im, labels, p=1.0):
         if self.transform and random.random() < p:
             new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
